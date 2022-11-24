321978c438590f64788a91c5b543e7f0c82aadda
MdRoki2020 <mroki815@gmail.com>
2022-11-24 21:17:01 +0600

    lab practice

---
+++ LabPractice/app/src/main/java/com/example/labpractice/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.labpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.textViewId);
+
+        textView.setText("Roki");
+    }
+}
